6efe20cfc631f66acd7bbf25ea97e6f4003016ef
BhavanaBandi <143982326+BhavanaBandi@users.noreply.github.com>
2025-03-11 20:57:04 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "I am PES1UG22AM040 and i finished lab 8 \n";
+  cout << "I love CC lab activities \n";
+  return 0;
+}
